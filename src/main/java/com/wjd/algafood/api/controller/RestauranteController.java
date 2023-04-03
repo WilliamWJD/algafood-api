a0a4fec0e660fd87e74e802a98daace3eb67132f
WilliamWJD <william.jose.wjd@gmail.com>
2023-04-03 08:57:16 -0300

    feat: implementando atualização de restaurante

--- src/main/java/com/wjd/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/wjd/algafood/api/controller/RestauranteController.java
@@ -7,6 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,21 +20,40 @@ import com.wjd.algafood.domain.service.RestauranteService;
 @RestController
 @RequestMapping("/restaurantes")
 public class RestauranteController {
-	
+
 	@Autowired
 	private RestauranteService restauranteService;
-	
+
 	@GetMapping
-	public List<Restaurante> listar(){
+	public List<Restaurante> listar() {
 		return restauranteService.listar();
 	}
-	
+
 	@GetMapping("/{restauranteId}")
-	public ResponseEntity<Restaurante> buscar(@PathVariable(name = "restauranteId") final Long restauranteId){
+	public ResponseEntity<Restaurante> buscar(@PathVariable(name = "restauranteId") final Long restauranteId) {
 		try {
 			return ResponseEntity.ok(restauranteService.buscar(restauranteId));
 		} catch (EntidadeNaoEncontradaException e) {
 			return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity<?> salvar(@RequestBody Restaurante restaurante) {
+		try {
+			return ResponseEntity.status(HttpStatus.CREATED).body(restauranteService.salvar(restaurante));
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
+		}
+	}
+	
+	@PutMapping("/{restauranteId}")
+	public ResponseEntity<?> atualizar(@PathVariable(name = "restauranteId") final Long restauranteId,
+			@RequestBody final Restaurante restaurante) {
+		try {
+			return ResponseEntity.status(HttpStatus.CREATED).body(restauranteService.atualizar(restaurante, restauranteId));
+		} catch (EntidadeNaoEncontradaException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
 		}
 	}
 }
--- src/main/java/com/wjd/algafood/domain/service/RestauranteService.java
+++ src/main/java/com/wjd/algafood/domain/service/RestauranteService.java
@@ -3,31 +3,58 @@ package com.wjd.algafood.domain.service;
 import java.util.List;
 import java.util.Objects;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.wjd.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.wjd.algafood.domain.model.Cozinha;
 import com.wjd.algafood.domain.model.Restaurante;
+import com.wjd.algafood.domain.repository.CozinhaRepository;
 import com.wjd.algafood.domain.repository.RestauranteRepository;
 
 @Service
 public class RestauranteService {
-	
+
 	@Autowired
 	private RestauranteRepository restauranteRepository;
-	
-	public List<Restaurante> listar(){
+
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
+
+	public List<Restaurante> listar() {
 		return restauranteRepository.listar();
 	}
-	
+
 	public Restaurante buscar(final long restauranteId) {
 		Restaurante restaurante = restauranteRepository.buscar(restauranteId);
-		
-		if(Objects.isNull(restaurante)) {
+
+		if (Objects.isNull(restaurante)) {
 			throw new EntidadeNaoEncontradaException(
 					String.format("Não existe um cadastro de restaurante com o código %d", restauranteId));
 		}
-		
+
 		return restaurante;
+	}
+
+	public Restaurante salvar(Restaurante restaurante) {
+		Cozinha cozinha = cozinhaRepository.buscar(restaurante.getCozinha().getId());
+
+		if (Objects.isNull(cozinha)) {
+			throw new EntidadeNaoEncontradaException(
+					String.format("Não foi encontrado uma cozinha com o id %d", restaurante.getCozinha().getId()));
+		}
+
+		restaurante.setCozinha(cozinha);
+
+		return restauranteRepository.salvar(restaurante);
+	}
+
+	public Restaurante atualizar(Restaurante restaurante, Long restauranteID) {
+		Restaurante restauranteAtual = buscar(restauranteID);
+
+		BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+		
+		return salvar(restauranteAtual);
 	}
 }
