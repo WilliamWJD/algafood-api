07f9e7937a91e5cf9e858108a1848bf899cb50f4
WilliamWJD <william.jose.wjd@gmail.com>
2023-04-08 12:09:27 -0300

    feat: parametros dinamicos para query sql

--- src/main/java/com/wjd/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
+++ src/main/java/com/wjd/algafood/infrastructure/repository/RestauranteRepositoryImpl.java
@@ -1,12 +1,16 @@
 package com.wjd.algafood.infrastructure.repository;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import com.wjd.algafood.domain.model.Restaurante;
 
@@ -18,10 +22,30 @@ public class RestauranteRepositoryImpl {
 
 	public List<Restaurante> find(String nome, BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal) {
 
-		String jpql = "from Restaurante where nome like :nome and taxaFrete between :taxaFreteInicial and :taxaFreteFinal";
+		StringBuilder jpql = new StringBuilder();
+		jpql.append("from Restaurante where 0 = 0 ");
+		
+		Map<String, Object> parametros = new HashMap<>();
 
-		return manager.createQuery(jpql, Restaurante.class).setParameter("nome", "%" + nome + "%")
-				.setParameter("taxaFreteInicial", taxaFreteInicial).setParameter("taxaFreteFinal", taxaFreteFinal)
-				.getResultList();
+		if (StringUtils.hasLength(nome)) {
+			jpql.append("and nome like :nome ");
+			parametros.put("nome",  "%" + nome + "%");
+		}
+
+		if (taxaFreteInicial != null) {
+			jpql.append("and taxaFrete >= :taxaFreteInicial ");
+			parametros.put("taxaFreteInicial", taxaFreteInicial);
+		}
+
+		if (taxaFreteFinal != null) {
+			jpql.append("and taxaFrete <= :taxaFreteFinal ");
+			parametros.put("taxaFreteFinal", taxaFreteFinal);
+		}
+		
+		TypedQuery<Restaurante> query = manager.createQuery(jpql.toString(), Restaurante.class);
+		
+		parametros.forEach((chave, valor) -> query.setParameter(chave, valor));
+		
+		return query.getResultList();
 	}
 }
